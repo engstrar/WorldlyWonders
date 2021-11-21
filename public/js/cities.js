241aec305bb8045b61f979d247c4415d5c2c5c5d
Arek Engstrom <engstrar@oregonstate.edu>
2021-11-21 08:55:19 -0700

    Clean-up unused files

--- public/js/cities.js
+++
@@ -1,9 +0,0 @@
-// Event listener for editing a city
-const addBrewery = document.querySelector("#addBrewery");
-addBrewery.addEventListener("submit", function (e) {
-	e.preventDefault();
-	let breweryName = addBrewery.breweryName.value;
-	let cityID = addBrewery.cityID.value;
-	console.log("It worked!!", breweryName, cityID);
-	addBrewery.reset();
-});
--- public/js/specialties.js
+++
@@ -1,8 +0,0 @@
-// Event listener for the addition of a new tap house
-const addSpecialty = document.querySelector("#addSpecialty");
-addSpecialty.addEventListener("submit", function (e) {
-	e.preventDefault();
-	let specialtyName = addSpecialty.specialtyName.value;
-	let description = addSpecialty.description.value;
-	addSpecialty.reset();
-});
